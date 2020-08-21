9ae4f77650642bc09f048bc0263810acaa72d487
rsad1234 <rsadle200@caledonian.ac.uk>
2020-08-22 00:14:21 +0100

    Adding Promises

--- js/sql.js
+++ js/sql.js
@@ -12,28 +12,38 @@ class SQL
 {
     QueryUser(userid)
     {
-        var sql = "SELECT * FROM users where user_id = ?";
-        connection.query(sql, [userid], function(err, rows)
+        return new Promise((resolve, reject) =>
         {
-            console.log(rows[0]);
-            return rows[0].results;
+            var sql = "SELECT * FROM users where user_id = ?";
+            connection.query(sql, [userid] , function(err, rows)
+            {
+                if (err)
+                    reject(err);
+                resolve(rows);
+            });
         });
     }
     QueryUserName(username)
     {
-        var sql = "SELECT * FROM users where name = ?";
-        connection.query(sql, [username]  , function(err, rows)
+        return new Promise((resolve, reject) =>
         {
-            return rows[0].username
+            var sql = "SELECT * FROM users where name = ?";
+            connection.query(sql, [username]  , function(err, rows)
+            {
+                return rows[0].username
+            });
         });
     }
     CreateUser(username, password, admin)
     {
-        var sql = "INSERT INTO users (password, username, admin) VALUES (?, ?, ?)";
-        connection.query(sql, [username, password, admin], function(err, rows)
+        return new Promise((resolve, reject) =>
         {
-            console.log(rows.user_id);
-            return rows.user_id;
+            var sql = "INSERT INTO users (password, username, admin) VALUES (?, ?, ?)";
+            connection.query(sql, [username, password, admin], function(err, rows)
+            {
+                console.log(rows.user_id);
+                return rows.user_id;
+            });
         });
     }
 }
