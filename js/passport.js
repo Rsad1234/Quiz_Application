90bf30f3c4b4d82649c89fec8af40d8f470d1ae1
rsad1234 <rsadle200@caledonian.ac.uk>
2020-08-22 00:58:20 +0100

    Added promises. Passport Register functionality Implemented

---
+++ js/passport.js
@@ -0,0 +1,64 @@
+// config/passport.js
+// load all the things we need
+var LocalStrategy   = require('passport-local').Strategy;
+//SQL Initialise START
+var SQL = require('./sql')
+let sql = new SQL
+//SQL STOP
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+module.exports = function(passport)
+{
+    // required for persistent login sessions
+    // passport needs ability to serialize and unserialize student out of session
+
+    // used to serialize the user for the session
+    passport.serializeUser(function(user, done)
+    {
+        done(null, user);
+    });
+    passport.deserializeUser(function(user, done)
+    {
+        sql.QueryUser(user).then((result) =>
+        {
+            done(err, result[0].userid)
+        }).catch((error) =>
+        {
+            console.log(error);
+        });
+    });
+
+
+    passport.use('local-signup', new LocalStrategy({
+        // by default, local strategy uses username and password, we will override with username
+        usernameField : 'username',
+        passwordField : 'password',
+        passReqToCallback : true // allows us to pass back the entire request to the callback
+    },
+    function(req, username, password, done) {
+        // find a user whose username is the same as the forms username
+        // we are checking to see if the user trying to login already exists
+        sql.QueryUserName(username).then((result) =>
+        {
+            if(result.length == 0 || result == undefined)
+            {
+                console.log("No user exists with this username |||| Creating User.....");
+                var NewUser = new Object();
+                NewUser.username = username;
+                NewUser.password = password;
+                NewUser.admin = 0;
+                return done(null, sql.CreateUser(NewUser.username, NewUser.password, NewUser.admin));
+            }
+            else
+            {
+                console.log("User Already Exists!");
+                return done(null, false);
+            }
+        }).catch((error) =>
+        {
+            console.log(error);
+        });
+
+    }));
+}
--- js/sql.js
+++ js/sql.js
@@ -1,4 +1,5 @@
-var mysql = require("mysql")
+var mysql = require("mysql");
+const { doesNotMatch } = require("assert");
 
 var connection = mysql.createConnection({
     host     : 'localhost',
@@ -14,7 +15,7 @@ class SQL
     {
         return new Promise((resolve, reject) =>
         {
-            var sql = "SELECT * FROM users where user_id = ?";
+            var sql = "SELECT * FROM users WHERE user_id = ?";
             connection.query(sql, [userid] , function(err, rows)
             {
                 if (err)
@@ -27,24 +28,24 @@ class SQL
     {
         return new Promise((resolve, reject) =>
         {
-            var sql = "SELECT * FROM users where name = ?";
-            connection.query(sql, [username]  , function(err, rows)
+            var sql = "SELECT * FROM users WHERE username = ?";
+            connection.query(sql, [username], function(err, rows)
             {
-                return rows[0].username
+                if (err)
+                {
+                    reject(err);
+                }
+                resolve(rows);
             });
         });
     }
     CreateUser(username, password, admin)
     {
-        return new Promise((resolve, reject) =>
-        {
-            var sql = "INSERT INTO users (password, username, admin) VALUES (?, ?, ?)";
+            var sql = "INSERT INTO users (username, password, admin) VALUES (?, ?, ?)";
             connection.query(sql, [username, password, admin], function(err, rows)
             {
-                console.log(rows.user_id);
-                return rows.user_id;
+                return;
             });
-        });
     }
 }
 module.exports = SQL;
